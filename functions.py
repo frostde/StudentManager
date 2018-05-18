1b88ae11cac3e095b4585cb7f4cddba131b84790
danielfrost <frostde93@gmail.com>
2018-05-18 16:30:00 -0400

    added logic for adding students, and changed a student to a dict

--- functions.py
+++ functions.py
@@ -3,18 +3,26 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student.title()
+        students_titlecase = student["name"].title()
     return students_titlecase
 
+
 def print_students_titlecase():
-    student_titlecase = get_students_titlecase()
+    students_titlecase = get_students_titlecase()
     print(students_titlecase)
 
-def add_student(name):
-    students.append(name)
+
+def add_student(name, student_id=332):
+    student = { "name": name, "student_id": student_id }
+    students.append(student)
+
 
 student_list = get_students_titlecase()
 
-add_student("Daniel")
+student_name = input("Enter student name:")
+student_id = input("Enter student id:")
 
-print(students)
+add_student(student_name, student_id)
+print_students_titlecase()
+
+
