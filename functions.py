2ad2a98cca1bf1bd21e7f61f95b396cacea366f1
danielfrost <frostde93@gmail.com>
2018-05-18 16:08:05 -0400

    commiting from visual studio code

--- functions.py
+++ functions.py
@@ -7,12 +7,14 @@ def get_students_titlecase():
     return students_titlecase
 
 def print_students_titlecase():
-    students_titlecase = []
-    for student in students:
-        students_titlecase = student.title()
+    student_titlecase = get_students_titlecase()
     print(students_titlecase)
 
 def add_student(name):
     students.append(name)
 
 student_list = get_students_titlecase()
+
+add_student("Daniel")
+
+print(students)
